55799cfdb3d358d641dbe14431d7e410d676ae26
rolandm99 <manfulmweze99@gmail.com>
2022-04-29 09:27:03 +0100

    Update project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/navBar';
-import Home from './components/items/itemList';
+import Home from './components/Home';
+import Items from './components/items/itemList';
+import Reserves from './components/reservations/reserveList';
 
 const App = () => (
   <BrowserRouter>
     <Navigation />
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/Doctors" element={<Items />} />
+      <Route exact path="/reservations" element={<Reserves />} />
     </Routes>
   </BrowserRouter>
 );
