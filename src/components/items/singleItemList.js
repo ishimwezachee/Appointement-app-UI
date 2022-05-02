5bcde13a6cd5510cebbfc7627328b8bd4037b13a
aakbarkhan <aakbarkhan@gmail.com>
2022-05-02 18:39:15 +0530

    Ceated a single item component to pass props

---
+++ src/components/items/singleItemList.js
@@ -0,0 +1,52 @@
+/* eslint-disable react/jsx-key */
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import SingleItems from './singleItem';
+
+import { getItemFromApi } from '../../redux/singleItemReducer';
+
+const SingleItem = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  // console.log(id, 'Params id');
+  const singleItem = useSelector((state) => state.singleitemReducer);
+  console.log(singleItem.name, 'single item prop..');
+  useEffect(() => {
+    dispatch(getItemFromApi(id));
+  }, []);
+  //   console.log((item[0]), 'I am displaying single item');
+  // console.log(SingleItem, 'this is single item...');
+  //  const id = document.getElementById('id');
+  //   console.log(id, 'this is id in reducer');
+
+  // const itemsList = SingleItem ? SingleItem.map((item) => (
+  //   <ul>
+  //     <li key={item.id}>{item.name}</li>
+  //     <li key={item.title}>{item.title}</li>
+  //     <li key={item.description}>{item.description}</li>
+  //   </ul>
+  // )) : <p>Loading....</p>;
+
+  return (
+    <>
+      <div>
+        <h1>Single Item</h1>
+        <SingleItems
+          id={singleItem.id}
+          key={singleItem.id}
+          title={singleItem.title}
+          name={singleItem.name}
+          description={singleItem.description}
+          image={singleItem.image}
+        />
+        {/* <img src={item[0].image} alt={item[0].name} />
+        {/* {(item).map((ele) => <li id={ele.id} key={1}>{`hi ${ele.name}`}</li>)} */}
+        <button type="button" onClick={() => { window.location.href = '/reservatonForm'; }}>Clicked to fixed an appointment</button>
+      </div>
+
+    </>
+  );
+};
+
+export default SingleItem;
