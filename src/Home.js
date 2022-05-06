738d3cef4f12c13d9efa9df1a422856c8ee09e1e
rolandm99 <manfulmweze99@gmail.com>
2022-05-06 10:35:25 +0100

    Update the welcome page

--- src/Home.js
+++ src/Home.js
@@ -8,17 +8,19 @@ const Home = () => (
     <div className="my-logo">
       <img src={logo} className="App-logo" alt="logo" width="220px" height="160px" />
     </div>
-    <div className="presentation">
-      <span>We Provide All Health Care Solution</span>
-      <h2>Protect Your Health And Take Care To Of Your Health</h2>
-    </div>
     <div className="enter">
-      <button type="button" className="join-log">
-        <Link id="li" to="/login">Log In</Link>
-      </button>
-      <button type="button" className="join-sign">
-        <Link id="li" to="/signup">Sign Up</Link>
-      </button>
+      <div className="presentation">
+        <span className="span">We Provide All Health Care Solution</span>
+        <h2 className="h2-pro">Protect Your Health And Take Care To Of Your Health</h2>
+      </div>
+      <div className="b-btn">
+        <button type="button" className="join-log">
+          <Link id="li" to="/login">Log In</Link>
+        </button>
+        <button type="button" className="join-sign">
+          <Link id="li" to="/signup">Sign Up</Link>
+        </button>
+      </div>
     </div>
   </div>
 );
