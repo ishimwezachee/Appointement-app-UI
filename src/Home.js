73cb165d2d85027521f326af7ea2d27f4cf111d9
rolandm99 <manfulmweze99@gmail.com>
2022-05-06 07:56:53 +0100

    Design the home page for our app

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,25 @@
 // client/src/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './assets/images/logo.png';
 
 const Home = () => (
-  <div>
-    <Link to="/login">Log In</Link>
-    <br />
-    <Link to="/signup">Sign Up</Link>
+  <div className="home">
+    <div className="my-logo">
+      <img src={logo} className="App-logo" alt="logo" width="220px" height="160px" />
+    </div>
+    <div className="presentation">
+      <span>We Provide All Health Care Solution</span>
+      <h2>Protect Your Health And Take Care To Of Your Health</h2>
+    </div>
+    <div className="enter">
+      <button type="button" className="join-log">
+        <Link id="li" to="/login">Log In</Link>
+      </button>
+      <button type="button" className="join-sign">
+        <Link id="li" to="/signup">Sign Up</Link>
+      </button>
+    </div>
   </div>
 );
 export default Home;
