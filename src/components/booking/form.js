a89674744dd193506b504f4090e3e3ea639d60dd
rolandm99 <manfulmweze99@gmail.com>
2022-05-06 07:57:47 +0100

    Update frontend part and add style for components

--- src/components/booking/form.js
+++ src/components/booking/form.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { getUsersFromApi } from '../../redux/userReducer';
 import { addBookToApi } from '../../redux/bookingReducer';
 // import { getItemFromApi } from '../../redux/singleItemReducer';
+import Nav from '../nav';
 
 const BookItem = () => {
   const [startDate, setStartDate] = useState('');
@@ -35,23 +36,27 @@ const BookItem = () => {
     setVisitType(e.target.value);
   };
   return (
-    <div className="form-div">
-      <hr />
-      <h3 className="new-book">Book Item </h3>
-      <form>
-        <input className="inp" type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} placeholder="Book title" required />
-        <input className="inp" type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} placeholder="Book title" required />
-        <select className="cate" name="Visits" id="book" onChange={visitTypeHandler} required>
-          <option>Select</option>
-          <option value="Headache">Headache</option>
-          <option value="Check-up">Check-u</option>
-          <option value="Teeth">Teeth</option>
-          <option value="HIV/AIDS">HIV/AID</option>
-          <option value="Therapy">Therapy</option>
-          <option value="Not sick">Not sick</option>
-        </select>
-        <input className="adding" type="button" value="Book Item" onClick={submitReservesToStore} />
-      </form>
+    <div className="item-page">
+      <div className="bar-page">
+        <Nav />
+      </div>
+      <div className="book-form">
+        <h3 className="new-book">Book an appointment with a docotor </h3>
+        <form>
+          <input className="inp" type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} placeholder="Book title" required />
+          <input className="inp" type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} placeholder="Book title" required />
+          <select className="cate" name="Visits" id="book" onChange={visitTypeHandler} required>
+            <option>Select</option>
+            <option value="Headache">Headache</option>
+            <option value="Check-up">Check-u</option>
+            <option value="Teeth">Teeth</option>
+            <option value="HIV/AIDS">HIV/AID</option>
+            <option value="Therapy">Therapy</option>
+            <option value="Not sick">Not sick</option>
+          </select>
+          <input className="adding" type="button" value="Book Item" onClick={submitReservesToStore} />
+        </form>
+      </div>
     </div>
   );
 };
